593b65bd7abe149e9c9bfd52b7c16968beac8084
opatomic <j@opatomic.com>
2019-01-31 16:57:16 -0500

    fix comment

--- build/node.foot.js
+++ build/node.foot.js
@@ -5,12 +5,10 @@ function newClient(s) {
 
 	wrapper.write = function(b) {
 		// the node socket docs state that the return value from write() should indicate whether the buffer
-		// is fully copied. however, this seems to be incorrect. therefore, a new buffer must be allocated
-		// for the serializer.
+		// is fully copied. however, this seems to be incorrect. therefore, a copy is allocated.
 		// TODO: consider a buffer pool to reuse buffers (write() will invoke a callback when done?)
 		// TODO: back-pressure: this function could return true/false indicating whether stream is writable; store/use this somehow
 		s.write(BUFFERFROM(b));
-		//c.s.b = NEWBUF(c.s.b.length);
 	};
 
 	s.on("data", function(b) {
